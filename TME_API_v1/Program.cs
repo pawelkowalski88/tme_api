2fb1042accef9a2e37433f8de6754a61f865259e
Pawel <Pawel@PAWEL-T430>
2017-12-12 21:24:54 +0100

    Add project files.

---
+++ TME_API_v1/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+using System.Xml.Serialization;
+using System.Threading;
+
+namespace TME_API_v1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TME API Start\n");
+            List<String> ProductIdentifiers = new List<string>() { "NE555D", "1N4007-DC", "16CTU04PBF", "1N4001-DCO" };
+            ResponseModel.response Response = APIGetPrices(ProductIdentifiers);
+            PrintResults(Response);
+            Console.ReadKey();
+        }
+
+        static ResponseModel.response APIGetPrices(List<String> products)
+        {
+            string Url = "https://api.tme.eu/Products/GetPrices.xml";
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            HttpClient MainClient = new HttpClient();
+
+            List<KeyValuePair<string, string>> ParamList = CreateParametersList(products);
+
+            byte[] key = Encoding.ASCII.GetBytes("9cdcef850cfbdfa01b26");
+            byte[] input = Encoding.ASCII.GetBytes(CreateSignatureFromParameters(Url, ParamList));
+            string Signature = EncodeSignature(input, key);
+
+            ParamList.Add(new KeyValuePair<string, string>("ApiSignature", Signature));
+
+            FormUrlEncodedContent Content = new FormUrlEncodedContent(ParamList);
+            Task<HttpResponseMessage> HttpsResponseTask = Task.Run(() => MainClient.PostAsync(Url, Content));
+            HttpResponseMessage HttpsResponse = HttpsResponseTask.Result;
+
+            try
+            {
+                Task<Stream> ReadResponseStreamTask = Task.Run(() => HttpsResponse.Content.ReadAsStreamAsync());
+                return SerializeResponseContent(ReadResponseStreamTask.Result);
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                return null;
+            }
+        }
+
+
+        static string EncodeSignature(byte[] signaturebase, byte[] secret)
+        {
+            byte[] hashValue;
+            using (HMACSHA1 hmac = new HMACSHA1(secret))
+            {
+                hashValue = hmac.ComputeHash(signaturebase);
+            }
+            return Convert.ToBase64String(hashValue);
+        }
+
+        static ResponseModel.response SerializeResponseContent(Stream content)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(ResponseModel.response));
+            serializer.UnknownNode += new XmlNodeEventHandler(serializer_UnknownNode);
+            serializer.UnknownAttribute += new XmlAttributeEventHandler(serializer_UnknownAttribute);
+            ResponseModel.response Response = (ResponseModel.response)serializer.Deserialize(content);
+            return Response;
+        }
+
+        static List<KeyValuePair<string, string>> CreateParametersList(List<string> parameterList)
+        {
+            List<KeyValuePair<string, string>> ParamList = new List<KeyValuePair<string, string>>();
+            foreach (var p in parameterList)
+            {
+               string SymbolIdentifier = String.Format("SymbolList[{0}]", ParamList.Count);
+                ParamList.Add(new KeyValuePair<string, string>(SymbolIdentifier.ToString(), p));
+            }
+
+            ParamList.Add(new KeyValuePair<string, string>("Country", "PL"));
+            ParamList.Add(new KeyValuePair<string, string>("Currency", "PLN"));
+            ParamList.Add(new KeyValuePair<string, string>("Language", "PL"));
+            ParamList.Add(new KeyValuePair<string, string>("Token", "dc26fdaad5a1bbcff9858b2161f54fe964c94b43e7a25"));
+
+            ParamList.Sort((x, y) => x.Key.CompareTo(y.Key));
+            return ParamList;
+        }
+
+        static string CreateSignatureFromParameters(string url, List<KeyValuePair<string, string>> paramList)
+        {
+            StringBuilder EncodedParameters = new StringBuilder();
+            foreach (var p in paramList)
+            {
+                if (paramList.IndexOf(p) != 0)
+                {
+                    EncodedParameters.Append("&");
+                }
+                EncodedParameters.Append(WebUtility.UrlEncode(p.Key));
+                EncodedParameters.Append("=");
+                EncodedParameters.Append(WebUtility.UrlEncode(p.Value));
+            }
+
+            StringBuilder SignatureBase = new StringBuilder();
+            SignatureBase.Append("POST&");
+            SignatureBase.Append(WebUtility.UrlEncode(url));
+            SignatureBase.Append("&");
+            SignatureBase.Append(WebUtility.UrlEncode(EncodedParameters.ToString()));
+
+            return SignatureBase.ToString();
+        }
+
+        static void PrintResults(ResponseModel.response response)
+        {
+            if (response.Data.ProductList == null) return;
+            foreach (var p in response.Data.ProductList)
+            {
+                Console.WriteLine(p.Symbol);
+                Console.WriteLine();
+
+                foreach (var price in p.PriceList)
+                {
+                    Console.WriteLine(price.Amount + "; " + price.PriceValue);
+                }
+                Console.WriteLine("\n");
+            }
+        }
+
+        protected static void serializer_UnknownNode(object sender, XmlNodeEventArgs e)
+        {
+            Console.WriteLine("Unknown Node:" + e.Name + "\t" + e.Text);
+        }
+
+        protected static void serializer_UnknownAttribute(object sender, XmlAttributeEventArgs e)
+        {
+            System.Xml.XmlAttribute attr = e.Attr;
+            Console.WriteLine("Unknown attribute " +
+            attr.Name + "='" + attr.Value + "'");
+        }
+    }
+}
---
+++ TME_API_v1/ResponseModel.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TME_API_v1
+{
+    public class ResponseModel
+    {
+        /// <remarks/>
+        [System.SerializableAttribute()]
+        [System.ComponentModel.DesignerCategoryAttribute("code")]
+        [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+        [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+        public partial class response
+        {
+
+            private string statusField;
+
+            private responseData dataField;
+
+            /// <remarks/>
+            public string Status
+            {
+                get
+                {
+                    return this.statusField;
+                }
+                set
+                {
+                    this.statusField = value;
+                }
+            }
+
+            /// <remarks/>
+            public responseData Data
+            {
+                get
+                {
+                    return this.dataField;
+                }
+                set
+                {
+                    this.dataField = value;
+                }
+            }
+        }
+
+        /// <remarks/>
+        [System.SerializableAttribute()]
+        [System.ComponentModel.DesignerCategoryAttribute("code")]
+        [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+        public partial class responseData
+        {
+
+            private string currencyField;
+
+            private string languageField;
+
+            private string priceTypeField;
+
+            private responseDataProduct[] productListField;
+
+            /// <remarks/>
+            public string Currency
+            {
+                get
+                {
+                    return this.currencyField;
+                }
+                set
+                {
+                    this.currencyField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string Language
+            {
+                get
+                {
+                    return this.languageField;
+                }
+                set
+                {
+                    this.languageField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string PriceType
+            {
+                get
+                {
+                    return this.priceTypeField;
+                }
+                set
+                {
+                    this.priceTypeField = value;
+                }
+            }
+
+            /// <remarks/>
+            [System.Xml.Serialization.XmlArrayItemAttribute("Product", IsNullable = false)]
+            public responseDataProduct[] ProductList
+            {
+                get
+                {
+                    return this.productListField;
+                }
+                set
+                {
+                    this.productListField = value;
+                }
+            }
+        }
+
+        /// <remarks/>
+        [System.SerializableAttribute()]
+        [System.ComponentModel.DesignerCategoryAttribute("code")]
+        [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+        public partial class responseDataProduct
+        {
+
+            private string symbolField;
+
+            private responseDataProductPrice[] priceListField;
+
+            private string unitField;
+
+            private byte vatRateField;
+
+            private string vatTypeField;
+
+            /// <remarks/>
+            public string Symbol
+            {
+                get
+                {
+                    return this.symbolField;
+                }
+                set
+                {
+                    this.symbolField = value;
+                }
+            }
+
+            /// <remarks/>
+            [System.Xml.Serialization.XmlArrayItemAttribute("Price", IsNullable = false)]
+            public responseDataProductPrice[] PriceList
+            {
+                get
+                {
+                    return this.priceListField;
+                }
+                set
+                {
+                    this.priceListField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string Unit
+            {
+                get
+                {
+                    return this.unitField;
+                }
+                set
+                {
+                    this.unitField = value;
+                }
+            }
+
+            /// <remarks/>
+            public byte VatRate
+            {
+                get
+                {
+                    return this.vatRateField;
+                }
+                set
+                {
+                    this.vatRateField = value;
+                }
+            }
+
+            /// <remarks/>
+            public string VatType
+            {
+                get
+                {
+                    return this.vatTypeField;
+                }
+                set
+                {
+                    this.vatTypeField = value;
+                }
+            }
+        }
+
+        /// <remarks/>
+        [System.SerializableAttribute()]
+        [System.ComponentModel.DesignerCategoryAttribute("code")]
+        [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+        public partial class responseDataProductPrice
+        {
+
+            private ushort amountField;
+
+            private decimal priceValueField;
+
+            private byte priceBaseField;
+
+            private object specialField;
+
+            /// <remarks/>
+            public ushort Amount
+            {
+                get
+                {
+                    return this.amountField;
+                }
+                set
+                {
+                    this.amountField = value;
+                }
+            }
+
+            /// <remarks/>
+            public decimal PriceValue
+            {
+                get
+                {
+                    return this.priceValueField;
+                }
+                set
+                {
+                    this.priceValueField = value;
+                }
+            }
+
+            /// <remarks/>
+            public byte PriceBase
+            {
+                get
+                {
+                    return this.priceBaseField;
+                }
+                set
+                {
+                    this.priceBaseField = value;
+                }
+            }
+
+            /// <remarks/>
+            public object Special
+            {
+                get
+                {
+                    return this.specialField;
+                }
+                set
+                {
+                    this.specialField = value;
+                }
+            }
+        }
+
+
+    }
+}
